2eafae8b47ab73c67faa76343a47e71abe295cde
Danil Martyushev <martyusheff.danil@yandex.ru>
2024-07-03 21:41:37 +0300

    added models

---
+++ src/test/java/ru/praktikum/services/qa/scooter/models/Order.java
@@ -0,0 +1,121 @@
+package ru.praktikum.services.qa.scooter.models;
+
+import java.util.List;
+
+public class Order {
+
+
+    int id;
+    int track;
+    String firstName;
+    String lastName;
+    String address;
+    String metroStation;
+    String phone;
+    int rentTime;
+    String deliveryDate;
+    String comment;
+    List <String> color;
+
+    public Order() {
+    }
+
+    public Order(String firstName, String lastName, String address, String metroStation, String phone, int rentTime, String deliveryDate, String comment, List <String> color) {
+        this.lastName = lastName;
+        this.address = address;
+        this.metroStation = metroStation;
+        this.phone = phone;
+        this.rentTime = rentTime;
+        this.deliveryDate = deliveryDate;
+        this.comment = comment;
+        this.color = color;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTrack() {
+        return track;
+    }
+
+    public void setTrack(int track) {
+        this.track = track;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getMetroStation() {
+        return metroStation;
+    }
+
+    public void setMetroStation(String metroStation) {
+        this.metroStation = metroStation;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getRentTime() {
+        return rentTime;
+    }
+
+    public void setRentTime(int rentTime) {
+        this.rentTime = rentTime;
+    }
+
+    public String getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public void setDeliveryDate(String deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public List <String> getColor() {
+        return color;
+    }
+
+    public void setColor(List <String> color) {
+        this.color = color;
+    }
+}
